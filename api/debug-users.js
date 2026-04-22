35f5450776090f92537061bcdac45e80bfa7fa3d
W0X1C123 <chanmichaelsuguitan@gmail.com>
2026-04-22 21:38:26 +0800

    Protect debug users endpoint with secret

--- api/debug-users.js
+++ api/debug-users.js
@@ -9,6 +9,24 @@ module.exports = async (req, res) => {
   }
 
   try {
+    const secret = process.env.DEBUG_USERS_SECRET;
+    const url = new URL(req.url, `https://${req.headers.host || 'localhost'}`);
+    const providedSecret = url.searchParams.get('secret');
+
+    if (!secret) {
+      return sendJson(res, 500, {
+        success: false,
+        message: 'Debug secret is not configured'
+      });
+    }
+
+    if (providedSecret !== secret) {
+      return sendJson(res, 403, {
+        success: false,
+        message: 'Forbidden'
+      });
+    }
+
     const users = await readUsers();
 
     return sendJson(res, 200, {
